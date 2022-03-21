a2b73018050621fb6ca373b7a1fccc5ea230984c
zhangyi <yi.zhang01@transwarp.io>
2022-03-21 23:46:55 +0800

    修改整个页面字体

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,23 @@ export default {
 };
 </script>
 <style lang="scss">
+@font-face {
+  font-family: "ali";
+  src: url("./assets/font/ali.ttf");
+}
+@font-face {
+  font-family: '演示秋鸿楷体';
+  src: url('./assets/font/演示秋鸿楷.ttf');
+}
+@font-face {
+  font-family: '思源宋体';
+  src: url('./assets/font/思源宋体.otf');
+}
+@font-face {
+  font-family: '思源黑体';
+  src: url('./assets/font/思源黑体 CN Regular.otf');
+}
+
 html,
 body {
   width: 100%;
@@ -23,7 +40,7 @@ body {
   padding: 0;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:'ali', '思源黑体','思源宋体', Avenir, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   // text-align: center;
